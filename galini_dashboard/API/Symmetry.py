30b293470e59a9f6d61e71a8c28c5143911a0923
Nicholas Li <nickyli98@gmail.com>
2019-03-07 22:28:10 +0000

    Symmetry tests

---
+++ galini_dashboard/API/Symmetry.py
@@ -0,0 +1,90 @@
+# Copyright 2018 Nicholas Li
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from collections import Counter
+from itertools import takewhile
+from galini_io.reader import MessageReader
+from google.protobuf.json_format import MessageToJson
+import h5py
+import json
+
+
+class Node:
+
+    def __init__(self, pos):
+        self.pos = pos
+        self.lower = None
+        self.upper = None
+        self.counts = None
+        self.freq = None
+
+    def setLower(self, lower):
+        self.lower = lower
+
+    def setUpper(self, upper):
+        self.upper = upper
+
+    def getCountsFreq(self):
+        return (self.counts, self.freq)
+
+    def calculateCounts(self):
+        assert len(self.lower) == len(self.upper)
+        self.counts = Counter(list(zip(self.lower, self.upper)))
+        self.freq = [0] * len(self.lower)
+        for (val, count) in self.counts.most_common():
+            self.freq[count - 1] += 1
+        self.freq = "".join(str(x) for x in self.freq)
+
+    def __str__(self):
+        return self.pos + "-" + str(self.counts)
+
+dic = dict()
+filename = "pointpack06"
+
+f = open(os.path.join(filename, "messages.bin"), "rb")
+reader = MessageReader(f)
+h5data = h5py.File(os.path.join(filename, "data.hdf5"), "r")
+for msg in reader:
+    if msg.HasField("tensor"):
+        msg_json = MessageToJson(msg)
+        data = h5data[msg.tensor.group_]
+        json_obj = json.loads(msg_json)
+        d = list(data[msg.tensor.dataset])
+        pos = json_obj['tensor']['group'].split('/')[-1]
+        ds = json_obj['tensor']['dataset']
+        if not ds == "solution":
+            if not pos in dic: 
+                dic[pos] = Node(pos)
+            if ds == "lower_bounds":
+                dic[pos].setLower(d)
+            elif ds == "upper_bounds":
+                dic[pos].setUpper(d)
+h5data.close()
+f.close()
+
+m = dict()
+
+for key, value in dic.items():
+    value.calculateCounts()
+    (counts, freq) = value.getCountsFreq()
+    if not freq in m:
+        m[freq] = []
+    m[freq].append(value)
+
+for key, value in m.items():
+    if len(value) > 1:
+        print(key)
+        eq = Counter([str(x.getCountsFreq()[0].most_common()) for x in value]).most_common()
+        print(list(takewhile(lambda x : x[1] > 1, eq)))
